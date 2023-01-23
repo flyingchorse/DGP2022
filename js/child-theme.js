4763fb58c1a14acfcd5c017965ef81b5c369b513
flyingchorse <dave@digidol-media.net>
2023-01-23 00:05:50 +0000

    tooltip

--- js/child-theme.js
+++ js/child-theme.js
@@ -7084,8 +7084,9 @@ jQuery(document).ready(function () {
 
   if (slide) {
     goToSlide(slide);
-  } //jQuery('[data-toggle="tooltip"]').tooltip();
+  }
 
+  jQuery('[data-toggle="tooltip"]').tooltip();
 });
 
 function goToSlide(number) {
